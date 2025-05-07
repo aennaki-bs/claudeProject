36e0b4fe1e8fb609437342afc3ee0160230f5f67
aennaki-bs <ahmed.hamudatech@gmail.com>
2025-05-07 12:04:42 +0100

    save changes

--- DocManagementBackend/obj/Debug/net9.0/DocManagementBackend.AssemblyInfo.cs
+++ DocManagementBackend/obj/Debug/net9.0/DocManagementBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocManagementBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd3edb2dc73924f9f6a178f7ca27083bf49c7e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64696ecee8aa4d8730b263b1ce7a575b10e5ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocManagementBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocManagementBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
